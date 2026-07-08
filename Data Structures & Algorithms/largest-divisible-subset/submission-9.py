29c6ac7f4631898092509acc5c8768230b7a1f92
Mihir Deshpande <77615094+mihirrd@users.noreply.github.com>
2026-07-08 12:56:22 -0400

    Add: largest-divisible-subset - submission-9

---
+++ Data Structures & Algorithms/largest-divisible-subset/submission-9.py
@@ -0,0 +1,27 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        cache = {}
+        def dfs(i):
+            if i >= len(nums):
+                return []
+            if i in cache:
+                return cache[i]
+            res = [nums[i]]
+            temp = []
+            for j in range(i+1, len(nums)):
+                if nums[j] % nums[i] == 0:
+                    temp = [nums[j]] + dfs(j)
+                if len(temp) > len(res):
+                    res = temp
+            
+            cache[i] = res
+            return res
+        
+        res = []
+        for i in range(len(nums)):
+            temp = dfs(i)
+            if len(temp) > len(res):
+                res = temp 
+        
+        return res
